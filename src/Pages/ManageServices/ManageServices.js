f3a4b20a93aee5cb9ebcea2fb7e07abb0e41c2a2
codeCampWithSifat <sayedhossainsifat300@.com>
2022-12-04 23:52:43 +0600

    full done

--- src/Pages/ManageServices/ManageServices.js
+++ src/Pages/ManageServices/ManageServices.js
@@ -1,11 +1,41 @@
-import React from 'react'
+import React from "react";
+import useServices from "../../hooks/useServices";
 
 const ManageServices = () => {
-  return (
-    <div className='container' style={{marginTop:"8rem"}}>
-        <h3>Manage Our Services</h3>
-    </div>
-  )
-}
+  const [services, setServices] = useServices();
 
-export default ManageServices
+  const handleDeleteService = (id) => {
+    if (window.confirm("Are You Sure You Want To Delete This One")) {
+      fetch(`http://localhost:5000/service/${id}`, {
+        method: "DELETE",
+      })
+        .then((res) => res.json())
+        .then((data) => {
+          if (data.deletedCount > 0) {
+            const restService = services.filter(
+              (service) => service._id !== id
+            );
+            setServices(restService);
+            alert("Deleted Your Service Successfully");
+          }
+        });
+    }
+  };
+  return (
+    <div className="container text-center" style={{ marginTop: "8rem" }}>
+      {services.map((service) => (
+        <div key={service._id}>
+          <h4>{service.name}</h4>
+          <button
+            onClick={() => handleDeleteService(service._id)}
+            className="btn btn-info"
+          >
+            X
+          </button>
+        </div>
+      ))}
+    </div>
+  );
+};
+
+export default ManageServices;
